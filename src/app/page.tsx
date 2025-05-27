f76ca053432003b269719677781e26b1133dca22
angeloluna <angel.luna.najera@gmail.com>
2025-05-27 00:24:16 -0600

    [fix] border on background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Advertorial from '@/app/components/Home/Advertorial'
 
 export default function Home() {
   return (
-    <div className="bg-[url('/images/background.png')] h-[96vh] border bg-repeat-x bg-top overflow-auto w-full ">
+    <div className="bg-[url('/images/background.png')] h-[96vh] bg-repeat-x bg-top overflow-auto w-full ">
       <div className="font-[family-name:var(--font-montserrat)] box-border p-[20px] md:pt-[33px] flex items-center flex-col animate-bg-pulse ">
 
         <Form />
